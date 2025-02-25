1cd863f30612245a591c5dd6dee330c586a97a1f
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-02-24 19:04:27 -0600

    First

--- comienza_con_vocal.py
+++ comienza_con_vocal.py
@@ -1,15 +1,5 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+A = input("Escriba una paqlabra : \n")
+if A.upper()[0]=='A'or A.upper()[0]=='E' or A.upper()[0]=='I'or A.upper()[0]=='O' or A.upper()[0]=='U' :
+    print(A, "comienza con una vocal")
+else :
+    print(A, "no comienza con una vocal")
